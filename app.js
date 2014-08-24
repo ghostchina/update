48dde0fa209d1f6d38d26756f93526c0f8a3f89f
liu.xing <kissliux@qq.com>
2014-08-25 00:10:02 +0800

    代码格式调整

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var multer           = require('multer');
-var env              = process.env.NODE_ENV || 'development';
+var express         = require('express');
+var path            = require('path');
+var favicon         = require('static-favicon');
+var logger          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var multer          = require('multer');
+var env             = process.env.NODE_ENV || 'development';
 
 var routes = require('./routes/index');
 var bookshelf = require('./db').bookshelf;
