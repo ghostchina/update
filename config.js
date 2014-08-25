c0ff77aed6f593f5d563eb080f14ee94e24440b8
wangsai <wangsai@bootcss.com>
2014-08-25 11:39:35 +0800

    根据当前环境导出配置

--- config.js
+++ config.js
@@ -3,16 +3,33 @@
  */
 var config = {
     development:{
+        port: 3000,
         database:{
             client: 'mysql',
             connection: {
                 host     : '127.0.0.1',
-                user     : 'liux',
-                password : 'liux',
-                database : 'ghost_update',
+                user     : 'root',
+                password : 'xxxxxx',
+                database : 'update',
                 charset  : 'utf8'
             }
-        }
+        },
+        currentVersion: "0.5.0"
+    },
+
+    production:{
+        port: 3000,
+        database:{
+            client: 'mysql',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'root',
+                password : 'xxxxxx',
+                database : 'update',
+                charset  : 'utf8'
+            }
+        },
+        currentVersion: "0.5.0"
     }
 }
-module.exports = config;
+module.exports = config[process.env.NODE_ENV || 'development'];
